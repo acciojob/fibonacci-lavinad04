026b3dda3e6c54336c79cc2f84f565af0c231ee9
Lavina <150042947+lavinad04@users.noreply.github.com>
2024-02-15 18:04:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	let num1 = 0;
+	let num2 = 1;
+	let num3 = num1+num2;
+
+	 for (let i = 0; i < num; i++) {
+	 	 num1 = num2;
+		 num2 = num3;
+		 num3 = num1+num2;
+	 }
 }
 
 module.exports = fibonacci;
